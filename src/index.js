618bc8f8d08fae2f73a2a24a11fddb6861682a28
Rohit <rohitpratti@gmail.com>
2019-07-16 13:08:40 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let listArray = [];
+
+  document.getElementById("create-task-form").addEventListener("submit", function(e) {
+    const todoButton = document.querySelector(".submitButton")
+    e.preventDefault();
+    let x = document.getElementById("new-task-description").innerText;
+    console.log(e);
+
+    node = document.createElement("LI")
+    let y = document.createTextNode(e.target[0].value);
+    node.appendChild(y);
+    document.getElementById("tasks").appendChild(node);
+  })
+
+  
+
+
 });
